76fbc82b5ff653c1eca1b85ca37bdd9248780c68
William Camacho <widacasan@gmail.com>
2023-06-12 19:08:36 -0500

    rename label

--- src/components/DataSelector.js
+++ src/components/DataSelector.js
@@ -3,8 +3,8 @@ import Select from 'react-select';
 
 function DataSelector({ onDataSelect }) {
     const options = [
-        { value: 'sales', label: 'Ventas' },
-        { value: 'users', label: 'Usuarios' },
+        { value: 'sales', label: 'datos de ventas por región' },
+        { value: 'users', label: 'datos de usuarios registrados por mes' },
         // ... más opciones ...
     ];
 
